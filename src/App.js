b0d7ea573f5b3e6255c6191b0b2ca4d8ab295ec7
Steven Lasch <91107394+s-lasch@users.noreply.github.com>
2023-08-30 18:24:35 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { ReactReader, ReactReaderStyle } from "react-reader";
  * Brisingr.epub                    - Swedish
  * Gulag_Archipelago.epub           - English
  */
-import Ebook from "./Humanism - a very short introduction.epub";
+import Ebook from "./humanism.epub";
 
 const ownStyles = {
   ...ReactReaderStyle,
